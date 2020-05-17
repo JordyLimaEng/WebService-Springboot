f261dfcbcdf2075a7ef4dac296ec7dff6e4d0a32
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 04:27:30 -0300

    exception handling delete

--- src/main/java/com/jordy/projetoweb/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/jordy/projetoweb/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jordy.projetoweb.services.exceptions.DatabaseException;
 import com.jordy.projetoweb.services.exceptions.ResourceNotFoundException.ResourceNotFoundException;
 
 @ControllerAdvice // intercepta as exceções para q o obj possa executar um possivel tratamento
@@ -21,4 +22,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)// mostra qual exceção o método vai interceptar e vai tratar oq tiver dentro do metodo
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
--- src/main/java/com/jordy/projetoweb/services/UserService.java
+++ src/main/java/com/jordy/projetoweb/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jordy.projetoweb.entities.User;
 import com.jordy.projetoweb.repositories.UserRepository;
+import com.jordy.projetoweb.services.exceptions.DatabaseException;
 import com.jordy.projetoweb.services.exceptions.ResourceNotFoundException.ResourceNotFoundException;
 
 @Service// registra a classe como um serviço
@@ -30,7 +33,13 @@ public class UserService {//camada de serviço
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
