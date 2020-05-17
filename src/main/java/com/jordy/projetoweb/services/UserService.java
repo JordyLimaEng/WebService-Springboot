42f9cbcfb54b1cfbe80e8297e5e5e83e97be02c7
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-17 03:41:48 -0300

    user delete

--- src/main/java/com/jordy/projetoweb/services/UserService.java
+++ src/main/java/com/jordy/projetoweb/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {//camada de serviço
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
